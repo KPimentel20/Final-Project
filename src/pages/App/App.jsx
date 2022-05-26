b6e7181f066175705479462e833a53e13ceceb55
KP Pimentel <kppimentel@Autumns-MBP.lan>
2022-05-25 21:04:53 -0400

    Finished setting up my App.jsx

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,53 +1,27 @@
 import React, { useState } from "react";
-import { Navigate, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import SignupPage from "../SignupPage/SignupPage";
 import LoginPage from "../LoginPage/LoginPage";
 import userService from "../../utils/userService";
+import PlaylistFeed from "../../components/PlaylistFeed/PlaylistFeed";
+import PlaylistPage from "../PlaylistPage/PlaylistPage";
 
 function App() {
-  const [user, setUser] = useState(userService.getUser()); // getUser decodes our JWT token, into a javascript object
-  // this object corresponds to the jwt payload which is defined in the server signup or login function that looks like
-  // this  const token = createJWT(user); // where user was the document we created from mongo
+  const [user, setUser] = useState(userService.getUser()); 
 
   function handleSignUpOrLogin() {
-    setUser(userService.getUser()); // getting the user from localstorage decoding the jwt
+    setUser(userService.getUser())
   }
 
-  function handleLogout() {
-    userService.logout();
-    setUser(null);
-  }
-
-  if (user) {
     return (
       <Routes>
-        <Route path="/" element={<h1>This is Home Page!</h1>} />
-        <Route
-          path="/login"
-          element={<LoginPage handleSignUpOrLogin={handleSignUpOrLogin} />}
-        />
-        <Route
-          path="/signup"
-          element={<SignupPage handleSignUpOrLogin={handleSignUpOrLogin} />}
-        />
+        <Route path="/" element={<PlaylistFeed user={user}/>} />
+        <Route path="/login" element={<LoginPage handleSignUpOrLogin={handleSignUpOrLogin} />} />
+        <Route path="/signup" element={<SignupPage handleSignUpOrLogin={handleSignUpOrLogin} />} />
+        <Route path="/playlist" element={<PlaylistPage user={user}/>} />
       </Routes>
     );
   }
-
-  return (
-    <Routes>
-      <Route
-        path="/login"
-        element={<LoginPage handleSignUpOrLogin={handleSignUpOrLogin} />}
-      />
-      <Route
-        path="/signup"
-        element={<SignupPage handleSignUpOrLogin={handleSignUpOrLogin} />}
-      />
-      <Route path="/*" element={<Navigate to="/login" />} />
-    </Routes>
-  );
-}
 
 export default App;
