ab75fcf8f1cb6cc50e893a4188e846526f1056f6
KP Pimentel <kppimentel@Autumns-MBP.lan>
2022-05-25 21:02:49 -0400

    Finished setting up my SignUp Page

--- src/pages/SignupPage/SignupPage.jsx
+++ src/pages/SignupPage/SignupPage.jsx
@@ -1,20 +1,88 @@
 import React, { useState } from "react";
 import ErrorMessage from "../../components/ErrorMessage/ErrorMessage";
-
+import { Button, Form, Header, Grid, Segment } from 'semantic-ui-react'
 import userService from "../../utils/userService";
 import { useNavigate } from "react-router-dom";
 
 export default function SignUpPage(props) {
+
+  const navigate = useNavigate()
+
+  const [error, setError] = useState('')
+  const [state, setState] = useState({
+    username: '',
+    email: '',
+    password: '',
+    passwordConf: ''
+  })
+
+  async function handleSubmit(e){
+    e.preventDefault()
+
+    try {
+      await userService.signup(state) 
+      props.handleSignUpOrLogin();
+      navigate('/')
+
+    } catch(err){
+      console.log(err.message);
+      setError(err.message)
+    }
+
+  }
+
+  function handleChange(e){
+    setState({
+      ...state, 
+      [e.target.name]: e.target.value
+    })
+  }
+
+
   return (
-    <>
-      <h1>Signup PAGE</h1>
-      <ul>
-        <li>Read the User Model, You can change it to fit your needs</li>
-        <li>
-          Make sure you read the Signup up func in the User Controller, to know
-          how it is setup to find the user!
-        </li>
-      </ul>
-    </>
+    <Grid textAlign="center" style={{ height: "100vh" }} verticalAlign="middle">
+      <Grid.Column style={{ maxWidth: 450 }}>
+        <Header textAlign="center">Sign Up</Header>
+        <Form autoComplete="off" onSubmit={handleSubmit}>
+          <Segment stacked>
+            <Form.Input
+              name="username"
+              placeholder="username"
+              value={state.username}
+              onChange={handleChange}
+              required
+            />
+            <Form.Input
+              type="email"
+              name="email"
+              placeholder="email"
+              value={state.email}
+              onChange={handleChange}
+              required
+            />
+            <Form.Input
+              name="password"
+              type="password"
+              placeholder="password"
+              value={state.password}
+              onChange={handleChange}
+              required
+            />
+            <Form.Input
+              name="passwordConf"
+              type="password"
+              placeholder="Confirm Password"
+              value={state.passwordConf}
+              onChange={handleChange}
+              required
+            />
+            <Button type="submit" className="btn">Signup</Button>
+          </Segment>
+          {error ? <ErrorMessage error={error} /> : null}
+        </Form>
+      </Grid.Column>
+    </Grid>
   );
 }
+
+
