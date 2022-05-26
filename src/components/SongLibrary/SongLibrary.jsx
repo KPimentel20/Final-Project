ba8f4e9738e4efd95355b1a0757c8d55033eeb2a
KP Pimentel <kppimentel@Autumns-MBP.lan>
2022-05-26 01:43:20 -0400

    Adding the Loader

--- src/components/SongLibrary/SongLibrary.jsx
+++ src/components/SongLibrary/SongLibrary.jsx
@@ -1,15 +1,31 @@
 import React from "react";
+import { Card, Dimmer, Segment, Image  } from 'semantic-ui-react'
 import SongCard from "../SongCard/SongCard";
+import Loader from '../Loader/Loader';
 
-export default function PlaylistFeed({ playlist, song, removeSong, addSong, user }) {
+export default function SongFeed({playlists, loading, numPhotosCol, song, removeSong, addSong, user }) {
 
   return (
-   <SongCard
-   song={song}
-   playlist={playlist}
-   addSong={addSong}
-   removeSong={removeSong}
-   user={user}
-    />
-  );
-}
+      <Card.Group itemsPerRow={numPhotosCol} stackable>
+        {loading ? (
+          <Segment>
+            <Dimmer active inverted>
+              <Loader size="small">Loading</Loader>
+            </Dimmer>
+            <Image src="https://react.semantic-ui.com/images/wireframe/short-paragraph.png" />
+          </Segment>
+        ) : null}
+        {playlists.map((playlist) => {
+          return (
+            <SongCard
+              song={song}
+              playlist={playlist}
+              addSong={addSong}
+              removeSong={removeSong}
+              user={user}
+            />
+          );
+        })}
+      </Card.Group>
+    )
+  }
